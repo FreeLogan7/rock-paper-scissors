ad63c031e4228c3bcdcd67551ef396008b5780c5
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-08-12 17:05:08 -0400

    created an adapter that uses a switch/case function to return computers string answer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,9 +3,22 @@
 var options = 3;
 
 function random(){
-let x = Math.ceil(Math.random() * options);
-return x;
+let randomNumber = Math.ceil(Math.random() * options);
+return randomNumber;
 }
 
-var ans = random();
-console.log(ans);
+//incomplete function for combining computer choice (allows for methods to be seperate)
+// function computerChoice (){
+//     let number = random()
+// }
+
+//This function adapts the random number (1-3) and converts to a choice
+function gameAdapter(num){
+   switch(num){
+        case(1): return console.log("Rock");
+        case(2): return console.log("Paper"); 
+        case(3): return console.log("Scissors"); 
+    }
+    return null;
+    
+}
