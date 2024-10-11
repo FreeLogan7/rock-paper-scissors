4fa8132eb5679f1b208f043fb24541198a0e4f39
Logan <logan@bb23684-l-vit.home>
2024-10-10 22:50:09 -0400

    created output element with line break

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,46 +5,24 @@ var humanPoints = 0;
 var computerPoints = 0;
 var humanChose ;
 var computerChose;
+attachEvent();
 
 
-
-function random(){
-let randomNumber = Math.ceil(Math.random() * options);
-return randomNumber;
+function attachEvent(){
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+button.addEventListener("click", () => {
+playRound(button.id);
+})
+});
 }
 
-
-//This function adapts the random number (1-3) and converts to a choice
-function gameAdapter(num){
-   switch(num){
-        case(1): return "Rock";
-        case(2): return "Paper"; 
-        case(3): return "Scissors"; 
-    }
-   
-}
-
-// function for combining computer choice (allows for methods to be seperate)
-function getComputerChoice (){
-    let number = random();
-    let computerChoice = gameAdapter(number);
-    return computerChoice;
-}
-
-function getHumanChoice (){
-    let humanChoice = prompt("Rock, Paper, or Scissors");
-    humanChoice = humanChoice;
-    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
-    return humanChoice;
-}
-
-
-function playRound(){
+function playRound(humanChoice){
 
 
         this.computerChose = getComputerChoice();
-        this.humanChose = getHumanChoice();
-        console.clear();
+        this.humanChose = humanChoice;
+//        console.clear();
 
         output();
 
@@ -55,11 +33,46 @@ function playRound(){
             else if (computerChose == "Rock" & humanChose == "Scissors") result (3)
             else if (computerChose == "Paper" & humanChose == "Rock") result (3)
             else if (computerChose == "Scissors" & humanChose == "Paper") result (3)
-
-
-
 }
 
+function output(){
+displayChoices();
+displayWinner();
+displayScore();
+}
+
+function displayChoices(){
+let div = document.getElementById("chosenDiv");
+
+const text1 = `You Chose: ${this.humanChose}`;
+const text2 = `Computer Chose: ${this.computerChose}`;
+
+if (!div){
+div = document.createElement("div");
+div.id = "chosenDiv";
+div.style.cssText = "color: blue; background: white;";
+document.body.appendChild(div);
+}
+
+ div.replaceChildren(
+        document.createTextNode(text1),
+        document.createElement("br"),
+        document.createTextNode(text2)
+    );
+}
+
+function displayWinner() {
+}
+
+function displayScore(){
+}
+
+// function for combining computer choice (allows for methods to be separate)
+function getComputerChoice (){
+    let number = random();
+    let computerChoice = gameAdapter(number);
+    return computerChoice;
+}
 
 
 function result(result){
@@ -83,3 +96,28 @@ function result(result){
          }
     }
 }
+
+
+function random(){
+let randomNumber = Math.ceil(Math.random() * options);
+return randomNumber;
+}
+
+//This function adapts the random number (1-3) and converts to a choice
+function gameAdapter(num){
+   switch(num){
+        case(1): return "Rock";
+        case(2): return "Paper";
+        case(3): return "Scissors";
+    }
+
+}
+
+//OLD -- UPDATE
+//function getHumanChoice (){
+//    let humanChoice = prompt("Rock, Paper, or Scissors");
+//    humanChoice = humanChoice;
+//    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+//    return humanChoice;
+//}
+
